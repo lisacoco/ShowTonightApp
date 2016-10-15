e4acb64b68298dad60af5dd3db0aeaa108603043
Lisa Cohen <lisa.cohen@polytechnique.edu>
2016-10-15 12:35:35 +0100

    application statique

--- www/js/app.js
+++ www/js/app.js
@@ -50,34 +50,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.shows', {
+      url: '/shows',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-shows': {
+          templateUrl: 'templates/tab-shows.html',
+          controller: 'ShowsCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('tab.show-detail', {
+      url: '/shows/:showId',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-shows': {
+          templateUrl: 'templates/show-detail.html',
+          controller: 'ShowDetailCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
