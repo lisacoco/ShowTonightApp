78ee421c8bd681b3d14a5453e73ed9582cc2dea1
Lisa Cohen <lisa.cohen@polytechnique.edu>
2016-10-20 18:18:39 +0200

    app mobile deux

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,10 +3,36 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('ShowsCtrl', function($scope, Shows) {
-  $scope.shows = Shows.all(); 
+  $scope.shows = [] ;
+  	Shows.all().then(function(apiShows) {
+  		$scope.shows = apiShows; 
+	});
 })
 
-.controller('ShowDetailCtrl', function($scope, $stateParams, Shows) {
+.controller('ShowDetailCtrl', function($scope, $stateParams, $ionicModal, Shows) {
   $scope.show = Shows.get($stateParams.showId);
+
+$ionicModal.fromTemplateUrl('templates/modal-book.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+}).then(function(modal) {
+    $scope.modal = modal;
+});
+
+$scope.openModal = function() {
+    $scope.modal.show();
+  };
+$scope.closeModal = function() {
+    $scope.modal.hide();
+};
+$scope.book = function(user_name, seats) {
+    return Shows.book($stateParams.showId, user_name, seats)
+    .then(function(booking) {
+      console.log("Booking", booking);
+      alert("Votre réservation a bien été prise en compte avec le numéro " + booking.id);
+      $scope.closeModal();
+    })
+  }
+
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,48 +1,19 @@
 angular.module('starter.services', [])
 
-.factory('Shows', function() {
+.factory('Shows', function($http) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-    var shows = [
-    {
-      id: 1,
-      name: "Mon premier Show",
-      location: "Salle Pleyel",
-      description: "Concert blabla",
-      capacity: 500,
-      price: 30,
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Salle_Pleyel_5.jpg/220px-Salle_Pleyel_5.jpg",
-      date: "2016-10-30",
-      url: "https://api-shows-tonight.herokuapp.com/shows/1.json"
-    },
-    {
-      id: 2,
-      name: "Sébastien Tellier",
-      location: "Nouveau Casino",
-      description: "L'Aventura",
-      capacity: 500,
-      price: 36,
-      image: "http://www.gqmagazine.fr/uploads/images/201421/cc/l_aventura_de_s__bastien_tellier_7651.jpeg",
-      date: "2016-10-19",
-      url: "https://api-shows-tonight.herokuapp.com/shows/2.json"
-    },
-    {
-      id: 5,
-      name: "Bertrand",
-      location: "Olympia Bruno Coquatrix",
-      description: "Concert de guitare classique",
-      capacity: 10,
-      price: 400,
-      image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Bob-Marley-in-Concert_Zurich_05-30-80.jpg",
-      date: "2016-11-05",
-      url: "https://api-shows-tonight.herokuapp.com/shows/5.json"
-    }
-  ];
+    var shows = [];
 
   return {
     all: function() {
-      return shows;
+      return $http.get("https://api-shows-tonight.herokuapp.com/shows.json")
+      .then(function(response) {
+        shows = response.data;
+          return shows;
+      })
+
     },
     get: function(showId) {
       for (var i = 0; i < shows.length; i++) {
@@ -51,6 +22,14 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+    book: function(showId, user_name, seats) {
+      return $http.post("https://api-shows-tonight.herokuapp.com/shows/" + showId + "/book.json", {booking: {user_name: user_name, seats: seats}}).then(function(response){
+        booking = response.data;
+        return booking;
+      });
     }
+
+
   };
 });
